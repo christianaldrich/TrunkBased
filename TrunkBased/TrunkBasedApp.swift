1a9e7f9307ac935af2d3024a45e41b99fd95acaf
christianaldrich <pang.aldrich@gmail.com>
2024-07-12 09:48:55 +0700

    Initial Commit

---
+++ TrunkBased/TrunkBasedApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrunkBasedApp.swift
+//  TrunkBased
+//
+//  Created by Christian Aldrich Darrien on 12/07/24.
+//
+
+import SwiftUI
+
+@main
+struct TrunkBasedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
